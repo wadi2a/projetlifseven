93289fef59c9dabdf9167d10ebeac1fc007befcf
thomas.zuppetti@orange.fr <thomas.zuppetti@orange.fr@0aed581c-194a-11de-9433-ad2e3fad96c5>
2009-03-25 16:11:50 +0000

    Module Terrain et Case terminé et fonctionnel. Reste à initialiser les terrains (Gilles ^^)

--- trunk/terrain.cpp
+++ trunk/terrain.cpp
@@ -1,31 +1,60 @@
 #include <stdlib.h>
-#include <iostream.h>
+#include <iostream>
 #include "terrain.h"
+
+
+Case * Terrain_Getcase(Terrain &t,const int &posx,const int &posy)
+{
+    return &t.plateau[posy*t.dim + posx]; //Accesseur pour simplifier l'accès au tableau 2D <=> t[x][y] et remplace t[x*t.dim +y] :)
+}
+
+void Terrain_Setcase(Terrain &t, const Case &c, const int &posx,const int &posy)
+{
+        t.plateau[posy*t.dim + posx] = c; // Mutateur
+}
 
 void Terrain_initialisation(Terrain & t, const int &niveau)
 {
     // Affectation tableaux pour chaque niveaux
-        Case * p1 = new Case n1[t.dim][t.dim] = {"vide","mur","brique"};
-        Case * p2 = new Case n2[t.dim][t.dim] = {"vide","mur"};
-        Case * p3 = new Case n3[t.dim][t.dim] = {"vide","mur"};
+        t.dim=2;
+        t.plateau = new Case[t.dim*t.dim]; // On prend un tableau 1D de taille 15*15, pour accéder à la positio (x,y) on effectue t[y*15 + x] Allouer un teableau 2D dynamiquement est compliqué !!!
 
+        /* Initialisation des tableaux pour chaque niveaux : c'est un tableau 1D que l'on utilise comme un 2D !!!
+        exemple pour un plateau 2x2 : on a un tableau 1D de 4 cases; les deux premieres cases correspondent à (0,0) et (0,1), les deux autres sont
+         (1,0) et (1,1) */
+
+         // Exemple pour test du module
         if (niveau == 1)
         {
-            t.plateau = p1;
+            Case a,b,c;
+            Case_initialisation(a);
+            strcpy(b.carre,"mursolide");
+            strcpy(c.carre,"mur");
+            Terrain_Setcase(t,a,0,0);
+            Terrain_Setcase(t,b,0,1);
+            Terrain_Setcase(t,a,1,0);
+            Terrain_Setcase(t,c,1,1);
+        }else{
+            Case a,b,c;
+            Case_initialisation(a);
+            strcpy(b.carre,"mursolide");
+            strcpy(c.carre,"mur");
+            Terrain_Setcase(t,b,0,0);
+            Terrain_Setcase(t,a,0,1);
+            Terrain_Setcase(t,a,1,0);
+            Terrain_Setcase(t,c,1,1);
 
-        }else{ if (niveau == 2)
-                {
-                    t.plateau = p2;
-
-                }else{
-
-                    t.plateau = p3;
-                }
         }
 }
 
 void Terrain_testament(Terrain &t)
 {
         delete t.plateau;
+}
+
+void Terrain_affichageCase(Terrain &t,const int &posx,const int &posy)
+{
+    Case * p = Terrain_Getcase(t,posx,posy);
+    printf("%s ",p->carre);
 }
 
--- trunk/terrain.h
+++ trunk/terrain.h
@@ -3,9 +3,10 @@
 #define _Terrain
 #include "case.h"
 
+
 struct sTerrain
 {
-     unsigned int dim = 15;
+     int dim;
      Case *plateau;
 
 };
@@ -14,10 +15,16 @@ typedef struct sTerrain Terrain;
 void Terrain_initialisation(Terrain & t, const int &niveau);
 // Initilialise le terrain en case vide et affecte les murs et murs solides en foncton du niveau.
 
-void Terrain_changeetat(Terrain &t, const int &posx, const int &posy, const);
-// Change l'état du terrain initialisé et supprime un mur (apparition du vide).
-
 void Terrain_testament(Terrain &t);
 // Vide correctement en mémoire le Terrain.
+
+Case * Terrain_Getcase(Terrain &t,const int &posx,const int &posy);
+// Accesseur du module terrain : on obtient une case
+
+void Terrain_affichageCase(Terrain &t,const int &posx,const int &posy);
+// Affichage avec printf de l'etat d'une case du plateau
+
+void Terrain_Setcase(Terrain &t, const Case &c, const int &posx,const int &poxy);
+// Accesseur du module terrain : modifie une case par une autre à une telle position
 
 #endif
