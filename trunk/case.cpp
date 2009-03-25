3a5519f51d9b5e69f539249fe2a48fc213862476
thomas.zuppetti@orange.fr <thomas.zuppetti@orange.fr@0aed581c-194a-11de-9433-ad2e3fad96c5>
2009-03-25 14:43:36 +0000

    Mise en ligne des premiers modules effectué mardi 24 mars 2009

---
+++ trunk/case.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <iostream>
+#include "case.h"
+
+void Case_initialisation(Case &a)
+{
+    strcopy(a.carre,"vide");
+}
+
+
+
---
+++ trunk/case.h
@@ -0,0 +1,15 @@
+#ifndef _Case
+#define _Case
+
+struct sCase
+{
+  char carre[20];
+};
+typedef struct sCase Case;
+
+void Case_initialisation(Case &a);
+/* Fonction qui initialise la case à vide */
+
+
+#endif
+
