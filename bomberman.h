e6ad7e1a42f8229fbb09494bbdaa20e83682f6e9
thomas.zuppetti <thomas.zuppetti@orange.fr>
2009-03-31 09:29:50 +0000

    Ajout de Bomberman.cpp

--- bomberman.h
+++ bomberman.h
@@ -11,23 +11,31 @@ struct sBomberman {
 };
 typedef struct sBomberman Bomberman;
 
+int Bomberman_Getposx(const Bomberman &b);
+
+int Bomberman_Getposy(const Bomberman &b);
+
+char Bomberman_Getdirection(const Bomberman &b);
+
+int Bomberman_Getbombe(const Bomberman &b);
+
+void Bomberman_Setbombe(Bomberman &b, const int &nb);
+
+void Bomberman_Setposx(Bomberman &b, const int &nb);
+
+void Bomberman_Setposy(Bomberman &b, const int &nb);
+
+void Bomberman_Setdirection(Bomberman &b, const int &nb);
+
 int Bombeman_Getnbvie(const Bomberman &b);
 
-void Bomberman_Getnbvie(Bomberman &b, const int &nbv);
+void Bomberman_Setnbvie(Bomberman &b, const int &nbv);
 
-void Bomberman_Incrementvie(Bomberman &b);
+void Bomberman_Incrementevie(Bomberman &b);
 
 void Bomberman_Initialisation(Bomberman &b);
 
 void Bomberman_mouvement(Bomberman &a, const Terrain &t, const char &dir);
 
 void Bomberman_testBombe(Bomberman &a, const Bombe &b);
-
-
-
-
-
-
-
-
 #endif
